32e41e915760b1f8ede1074fe08bf68b3f2742f5
Haerul Fajar <haerulr2@gmail.com>
2025-08-07 23:54:49 +0700

    Add ListBlog and CardBlog exports to components index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Hero } from './hero';
 export { default as Navbar } from './navbar';
+export { default as ListBlog } from './list-blog';
+export { default as CardBlog } from './card-blog';
