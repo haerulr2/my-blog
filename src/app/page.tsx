e4f78432a3ee0018024886122aae1f5ae28a5994
Haerul Fajar <haerulr2@gmail.com>
2025-08-17 14:36:06 +0700

    Format JSX structure in page.tsx for improved readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import { Footer, Hero, ListBlog, Navbar } from '@/components';
 export default function Home() {
   return (
     <main className='relative'>
-    <Navbar />
-    <Hero />
-    <ListBlog />
-    <Footer />
-  </main>
+      <Navbar />
+      <Hero />
+      <ListBlog />
+      <Footer />
+    </main>
   );
 }
