4c467466d623eae832d35e95b4d51f754f4b3697
haerulr2 <haerulr2@gmail.com>
2025-09-18 16:56:01 +0000

    Refactor CardBlog component to wrap content in a Link for improved navigation and user experience

--- src/app/components/card-blog.tsx
+++ src/app/components/card-blog.tsx
@@ -11,23 +11,24 @@ interface CardBlogProps {
 
 export default function CardBlog({ post }: CardBlogProps) {
   return (
-    <div className='bg-black/50 rounded-lg p-6 border border-neutral-500 relative'>
-      <Image
-        src={post.image}
-        loading='lazy'
-        className='rounded-lg mb-2'
-        alt={post.title}
-        width={600}
-        height={400}
-      />
-      <h3 className='text-lg font-bold mb-1'>{post.title}</h3>
-      <p className='text-neutral-400 mb-2'>{post.description}</p>
-      <Link
-        href={`/post/${post.slug}`}
-        className='absolute bottom-2 right-2 text-sm text-white hover:text-neutral-400 transition-colors flex items-center gap-2'
-      >
-        Read more <ArrowRight className='w-4 h-4' />
-      </Link>
-    </div>
+    <Link href={`/post/${post.slug}`}>
+      <div className='bg-black/50 rounded-lg p-6 border border-neutral-500 relative cursor-pointer'>
+        <Image
+          src={post.image}
+          loading='lazy'
+          className='rounded-lg mb-2'
+          alt={post.title}
+          width={600}
+          height={400}
+        />
+        <h3 className='text-lg font-bold mb-1'>{post.title}</h3>
+        <p className='text-neutral-400 mb-2'>{post.description}</p>
+        <div
+          className='absolute bottom-2 right-2 text-sm text-white hover:text-neutral-400 transition-colors flex items-center gap-2'
+        >
+          Read more <ArrowRight className='w-4 h-4' />
+        </div>
+      </div>
+    </Link>
   );
 }
